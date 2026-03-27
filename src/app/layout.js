76833cd6410f41e3dcd1612f962bc0adf0a5987a
ulysses <ulyssesgutierrez0221@gmail.com>
2026-03-27 01:15:43 -0500

    first didnt work, this is second attempt

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,8 +24,8 @@ export const metadata = {
   description:
     "McAllen North Seventh-day Adventist Church - A place of worship, community, and hope in McAllen, Texas.",
   icons: {
-    icon: "/adventist-logo.gif",
-    apple: "/adventist-logo.gif",
+    icon: "./public/adventist-logo.gif",
+    apple: "./public/adventist-logo.gif",
   },
 };
 
